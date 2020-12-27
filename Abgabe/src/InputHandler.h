c9d5b779703949970060b8eb75c95759f7d22b2b
tkideneb1999 <benedikt@familie-liebmann.de>
2020-12-27 20:09:41 +0100

    InputHandler modification

--- Abgabe/src/InputHandler.h
+++ Abgabe/src/InputHandler.h
@@ -19,19 +19,15 @@ namespace InputHandling {
 		IPE_PRESSED = 1,
 		IPE_REPEAT = 2
 	};
+
 	class InputHandler
 	{
 	private:
-		static std::vector<void(*)(const char*, InputPressEnum, InputModEnum)> m_KeyObservers;
-	public:
-		static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-		static void CursorPosCallback(GLFWwindow* window, double xPos, double yPos);
-		static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+		std::vector<void(*)(const char*, InputPressEnum, InputModEnum)> m_KeyObservers;
 
-		static int AddKeyObserver(void(*callback)(const char*, InputPressEnum, InputModEnum))
-		{
-			m_KeyObservers.emplace_back(callback);
-			return m_KeyObservers.size() - 1;
-		}
+	public:
+		void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		void CursorPosCallback(GLFWwindow* window, double xPos, double yPos);
+		void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
 	};
 }
