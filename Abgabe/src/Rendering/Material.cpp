d6ef6f7dc0de3b0eac1d4f92a5bc5d2f920e66ac
tkdieneb1999 <tkideneb44@gmail.com>
2021-01-03 12:35:37 +0100

    further implementation of Materials

--- Abgabe/src/Rendering/Material.cpp
+++ Abgabe/src/Rendering/Material.cpp
@@ -1,1 +1,26 @@
 #include "Material.h"
+
+Material::Material()
+{
+
+}
+
+Material::Material(const char* filePath)
+{
+
+}
+
+Material::Material(const Material& mat)
+{
+
+}
+
+Material::~Material()
+{
+
+}
+
+void Material::SetShader(const char* name)
+{
+
+}
--- Abgabe/src/Rendering/Material.h
+++ Abgabe/src/Rendering/Material.h
@@ -1,10 +1,31 @@
 #pragma once
 
+#include <unordered_map>
+#include <string>
+
+#include "glm/vec4.hpp"
+#include "glm/mat4x4.hpp"
+
 #include "Shader.h"
 
 class Material
 {
 private:
-	Shader* m_Shader;
+	std::string m_Name;
+	Shader* m_pShader;
+	std::unordered_map<const char*, int> m_IParameters;
+	std::unordered_map<const char*, float> m_FParameters;
+	std::unordered_map<const char*, glm::vec2> m_V2Parameters;
+	std::unordered_map<const char*, glm::vec3> m_V3Parameters;
+	std::unordered_map<const char*, glm::vec4> m_V4Parameters;
+	std::unordered_map<const char*, glm::mat4> m_M4Parameters;
 
+public:
+	Material();
+	Material(const char* filePath);
+	~Material();
+	Material(const Material& mat);
+
+	void SetShader(const char* shaderName);
+	Shader* GetShader() { return m_pShader; }
 };
